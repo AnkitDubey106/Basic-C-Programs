e063bbc980723ee5573efd90bb19cf5aa21e9516
Ankit Dubey <66075482+AnkitDubey106@users.noreply.github.com>
2021-12-04 16:39:05 +0530

    Add files via upload
    1. Write a program in C++ to find the first 10 natural numbers. Go to the editor
    Sample output:
    The natural numbers are:
    1 2 3 4 5 6 7 8 9 10

---
+++ 3questiononloop.cpp
@@ -0,0 +1,23 @@
+
+/*1. Write a program in C++ to find the first 10 natural numbers. Go to the editor and also their sum
+Sample output:
+The natural numbers are:
+1 2 3 4 5 6 7 8 9 10  55*/ 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{int countsum=0;
+int num;
+ for(int i=1;i<=10;i++)
+{
+    cout<<i<<endl;
+    countsum= i + countsum;
+
+}
+
+cout<<endl<<countsum;
+
+        return 0;
+}
