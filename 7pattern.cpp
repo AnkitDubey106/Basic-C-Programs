0d2505645ce93ef6a8b7214be287b2490722ca0f
Ankit Dubey <66075482+AnkitDubey106@users.noreply.github.com>
2021-12-19 13:34:07 +0530

    Add files via upload

---
+++ 7pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    for(char i='A';i<='H';i++)
+    {
+        for(char j='A';j<='H';j++)
+        {
+            cout<<j;
+            
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
