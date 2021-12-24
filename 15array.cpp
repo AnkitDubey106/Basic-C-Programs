e4dfcefea6bf0907c4ee725034bb50c0badb069f
Ankit Dubey <66075482+AnkitDubey106@users.noreply.github.com>
2021-12-24 16:30:46 +0530

    Add files via upload

---
+++ 15array.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+
+//1d array example
+int main()
+{
+    
+    int a[22]={1,3,4,-6,7,99,76,43,22};
+    cout<<"the array value at 3rd position is ="<<a[3];
+    
+    return 0;
+}
